d2806c32276cb97f1d652b796f9c8206341aa075
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-01 13:38:17 -0700

    Componente Title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,11 @@
+import { Title } from "@/components";
 import { titleFont } from "@/config/fonts";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hola mundo</h1>
-      <h1 className={titleFont.className}>Hola mundo</h1>
-    </div>
+    <>
+      <Title title="Tienda" subTitle="Todos los productos" className="mb-2"/>
+    </>
   );
 }
