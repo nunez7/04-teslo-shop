0e79946513ebfa9259ac37a6c5650eb956a35e8d
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-01 14:48:21 -0700

    Haciendo menú lateral

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -37,7 +37,7 @@ export const ProductGridItem = ( { product }: Props ) => {
           href={ `/product/${ product.slug }` }>
           { product.title }
         </Link>
-        <span className="font-bold">${ product.price }</span>
+        <span className="font-bold">${ product.price.toFixed(2) }</span>
       </div>
 
     </div>
