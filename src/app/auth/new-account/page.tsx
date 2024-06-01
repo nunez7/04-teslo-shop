68cfef76a0f3165bcadb01478c7aeb26ef9cb89b
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-01 11:38:07 -0700

    Estructura de app

---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from "@/config/fonts";
+import Image from "next/image";
+
+export default function NewAccountPage() {
+  return (
+    <main className="">
+      <h1>New Account</h1>
+    </main>
+  );
+}
