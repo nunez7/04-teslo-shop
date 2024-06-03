b36fa54a67353a6bf6ad2de9c0c64dc2714fba78
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-03 12:08:02 -0700

    Pantalla - Carrito vacio

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -2,6 +2,7 @@ import { QuantitySelector, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const productsInCart = [
   initialData.products[0],
@@ -10,6 +11,10 @@ const productsInCart = [
 ];
 
 export default function CartPage() {
+
+  //redirect('/empty');
+
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
 
