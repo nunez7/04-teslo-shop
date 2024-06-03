d36dbbb14cb9a9e886dcb294e4047f90f37c69ba
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-03 06:48:57 -0700

    Pantalla de categorias con filtro

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,23 +1,42 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props{
   params: {
-    id: string;
+    id: Category;
   }
 }
+const seedProducts = initialData.products;
 
 
 export default function CategoryPage({params}: Props) {
 
   const {id} = params;
 
-  if(id === "kids"){
+  if(id !== "kid" &&  id !=="men" && id !=="women"){
     notFound();
   }
 
+  const products = seedProducts.filter((product) =>{
+    return product.gender === id;
+  });
+
+  const labels: Record<Category, string> = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para niños',
+    'unisex': 'para todos'
+  }
+
   return (
-    <div>
-      <h1>Category Page {id} </h1>
-    </div>
+    <>
+      <Title title={`Articulos de ${labels[id]}`} subTitle={`Productos ${labels[id]}`} className="mb-2"/>
+
+      <ProductGrid 
+        products={ products }
+      />
+    </>
   );
 }
