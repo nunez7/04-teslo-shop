31ce15fc4c4144c04327e3909d96d10a11d342ec
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-04 10:51:14 -0700

    Paginacion - HTML

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import { getPaginatedProductsWithImages } from "@/actions";
-import { Title, ProductGrid } from "@/components";
+import { Title, ProductGrid, Pagination } from "@/components";
 
 interface Props{
   searchParams: {
@@ -13,7 +13,7 @@ export default async function Home({searchParams}: Props) {
 
   const page = searchParams.page ? parseInt(searchParams.page ) : 1;
 
-  const { products } = await getPaginatedProductsWithImages({page});
+  const { products, totalPages, currentPage } = await getPaginatedProductsWithImages({page});
 
   if(products.length === 0){
     redirect("/");
@@ -26,6 +26,8 @@ export default async function Home({searchParams}: Props) {
       <ProductGrid
         products={products}
       />
+
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
