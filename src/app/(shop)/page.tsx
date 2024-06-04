609579825a25eec435ca257c03bc369e38fa3514
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-04 10:00:52 -0700

    Controlando cuando no hay propductos

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { getPaginatedProductsWithImages } from "@/actions";
 import { Title, ProductGrid } from "@/components";
 
@@ -13,6 +14,10 @@ export default async function Home({searchParams}: Props) {
   const page = searchParams.page ? parseInt(searchParams.page ) : 1;
 
   const { products } = await getPaginatedProductsWithImages({page});
+
+  if(products.length === 0){
+    redirect("/");
+  }
 
   return (
     <>
