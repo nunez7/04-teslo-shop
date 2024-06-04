e86fb109ec82903a4bb080ed913a8d2fcda51fcf
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-04 15:02:12 -0700

    Optimizando metadata dinamica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,10 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
-  title: "Teslo | Shop",
+  title: {
+    template: '%s - Teslo | Shop',
+    default: 'Home - Teslo | Shop'
+  },
   description: "Tienda virtual de productos",
 };
 
