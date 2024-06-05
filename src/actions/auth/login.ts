26949d3b1490a489704ac97f220e62d98be7b9b4
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-05 12:16:08 -0700

    Servicios para validar el usuario

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,25 @@
+'use server';
+import { signIn } from '@/auth.config';
+import { AuthError } from 'next-auth';
+
+// ...
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        console.log(Object.fromEntries(formData));
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Credenciales invalidas.';
+                default:
+                    return 'Algo salió mal, intenta de nuevo.';
+            }
+        }
+        throw error;
+    }
+}
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,9 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import NextAuth,  {type NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import prisma from './lib/prisma';
+import bcryptjs from 'bcryptjs';
+
 export const authConfig: NextAuthConfig = {
     pages: {
         signIn: '/auth/login',
@@ -7,7 +11,33 @@ export const authConfig: NextAuthConfig = {
     },
     providers: [
 
-        
+        Credentials({
+            async authorize(credentials) {
+              const parsedCredentials = z
+                .object({ email: z.string().email(), password: z.string().min(6) })
+                .safeParse(credentials);
+
+                if(! parsedCredentials.success) return null;
+
+                const {email, password} = parsedCredentials.data;
+
+                //console.log({email, password});
+
+                //Buscar el corre
+                const user = await prisma.user.findUnique({where: {email: email.toLowerCase()}});
+                if(!user) return null;
+
+                //Comparar las contraseñas
+                if(!bcryptjs.compareSync(password, user.password)) return null;
+
+                //Regresar el usuario, menos su password
+                const {password: _, ...rest} = user;
+                console.log({rest});
+                return rest;
+            },
+          }),
 
     ]
 };
+
+export const {signIn, signOut, auth} = NextAuth (authConfig);
