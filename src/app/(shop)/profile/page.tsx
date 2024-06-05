90c53853b907cc149231a82902e94f2f636a2c69
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-05 13:00:03 -0700

    Diseño de perfil

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth.config";
 import { Title } from "@/components";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function NamePage() {
@@ -12,13 +13,23 @@ export default async function NamePage() {
     }
 
     return (
-        <div>
+        <>
             <Title title="Perfil de usuario" />
-            <pre>
-                {
-                    JSON.stringify(session.user, null, 2)
-                }
-            </pre>
-        </div>
+            <div className="bg-gray-200 font-sans h-screen w-full flex flex-row justify-center items-center">
+                <div className="card w-96 mx-auto bg-white shadow-md hover:shadow">
+                    <Image className="w-36 mx-auto rounded-full -mt-5 border-8 border-white" 
+                    width={200}
+                    height={200}
+                    src="https://avatars.githubusercontent.com/u/67946056?v=4" alt="" />
+                        <div className="text-center mt-2 text-3xl font-medium">{session.user.name}</div>
+                        <div className="text-center mt-2 font-light text-sm">{session.user.email}</div>
+                        <div className="px-6 text-center mt-2 font-light text-sm">
+                            <p>
+                                {JSON.stringify(session.user, null, 2)}
+                            </p>
+                        </div>
+                </div>
+            </div>
+        </>
     );
 }
