40ea89bdac5ca0bd9ec056e5e1b00255647187b2
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-05 16:56:16 -0700

    Fix para recargar cuando hace login, ya que no se refleja en el menu

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -3,19 +3,20 @@
 import { authenticate } from '@/actions';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+//import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 import { IoInformationOutline } from 'react-icons/io5';
 
 export const LoginForm = () => {
     const [state, dispatch] = useFormState(authenticate, undefined);
-    const router = useRouter();
+    //const router = useRouter();
 
     //Usamos el useEfect para redireccionar
     useEffect(() =>{
         if(state == 'Success'){
-            router.replace('/');
+            //router.replace('/');
+            window.location.replace("/");
         }
     }, [state]);
 
