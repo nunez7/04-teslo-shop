04a91e9079562d7f26f29b0e9c2316ee47ee8d20
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-05 13:20:43 -0700

    Cerrando sesión

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { logout } from '@/actions';
 import { useUIStore } from '@/store';
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -81,13 +82,13 @@ export const Sidebar = () => {
                     <span className='ml-4 text-xl'>Ingresar</span>
                 </Link>
 
-                <Link
-                    href="/logout"
-                    className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
+                <button
+                onClick={() => logout()}
+                    className='flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
                 >
                     <IoLogOutOutline size={30} />
                     <span className='ml-4 text-xl'>Salir</span>
-                </Link>
+                </button>
                 {/* Line separator */}
                 <div className='w-full h-px bg-gray-200 my-10' />
 
