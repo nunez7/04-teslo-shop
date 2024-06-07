8e80a5f8a0d3a5f19fae1209e5e421f53ade5825
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-06-07 11:52:30 -0700

    Creando pagos en paypal y actualizando el transactyionId

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,15 +1,51 @@
 'use client';
 
-import { PayPalButtons } from '@paypal/react-paypal-js';
 import React from 'react';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import { CreateOrderActions, CreateOrderData } from '@paypal/paypal-js';
+import { setTransactionId } from '@/actions';
 
 interface Props {
     orderId: string;
     amount: number;
-  }
+}
 
-  export const PayPalButton = ({ orderId, amount }: Props) => {
-  return (
-    <PayPalButtons />
-  )
+export const PayPalButton = ({ orderId, amount }: Props) => {
+
+    const [{ isPending }] = usePayPalScriptReducer();
+
+    const rountedAmount = (Math.round(amount * 100)) / 100; //123.23
+
+    if (isPending) {
+        return (
+            <div className="animate-pulse mb-16">
+                <div className="h-11 bg-gray-300 rounded" />
+                <div className="h-11 bg-gray-300 rounded mt-2" />
+            </div>
+        )
+    }
+
+    const createOrder = async (data: CreateOrderData, actions: CreateOrderActions): Promise<string> => {
+        const transactionId = await actions.order.create({
+            purchase_units: [
+                {
+                    invoice_id: orderId,
+                    amount: {
+                        value: `${rountedAmount}`,
+                    }
+                }
+            ]
+        });
+        const { ok } = await setTransactionId(orderId, transactionId);
+        if (!ok) {
+            throw new Error('No se pudo actualizar la orden');
+        }
+        return transactionId;
+    }
+
+    return (
+        <PayPalButtons
+            createOrder={createOrder}
+        />
+    )
 }
